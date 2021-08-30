790cfd37cb1118087905294a0dc76af78dd9ba26
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-08-30 16:38:01 +0300

    still need to figure out how to loop throug jasn to get all data

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,43 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 class HornedBeasts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfVotes: 0,
+    };
+  }
+  incrementNumberOfVotes = () => {
+    this.setState({
+      numberOfVotes: this.state.numberOfVotes + 1,
+    });
+  };
   render() {
     return (
       <>
-        <h2>{this.props.title}</h2>
+        {/* <h2>{this.props.title}</h2>
         <img src={this.props.imageUrl}></img>
-        <p> {this.props.description}</p>
+        <p> {this.props.description}</p> */}
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img
+            variant="top"
+            src={this.props.imageUrl}
+            onClick={this.incrementNumberOfVotes}
+          />
+
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              <p> {this.props.description}</p>
+              <p> Number Of Votes :{this.state.numberOfVotes}</p>
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </>
     );
   }
