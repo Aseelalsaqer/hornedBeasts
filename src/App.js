bf7759d1ffd3a3343633052c99412723440b1a28
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-09-04 18:48:42 +0300

    resloving lab03

--- src/App.js
+++ src/App.js
@@ -3,14 +3,57 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import Data from "./components/data.json";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SelectedBeast from "./components/SelectedBeast";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      title: '',
+      imageUrl: '',
+      description: '',
+      data: Data,
+    };
+  }
+  showModal = () => {
+    this.setState({
+      show: true,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      show: false,
+    });
+
+    
+  };
+  updateSelectedBeastData = (title, imageUrl, description) => {
+    this.setState({
+      title: title,
+      imageUrl: imageUrl,
+      description: description,
+    });
+  };
   render() {
     return (
       <>
         <Header />
 
-        <Main />
+        <Main
+          Data={Data}
+          showModal={this.showModal}
+          updateSelectedBeastData={this.updateSelectedBeastData}
+        />
+        <SelectedBeast
+          handleClose={this.handleClose}
+          show={this.state.show}
+          title={this.state.title}
+          imageUrl={this.state.imageUrl}
+          description={this.state.description}
+        />
 
         <Footer />
       </>
